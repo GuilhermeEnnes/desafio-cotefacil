51faec09994c0882d9ddd1e997503df54d50e383
(JAKSON) GuilhermeEnnes <jaksonguilherme2@gmail.com>
2025-07-31 05:34:19 -0300

    Criação do producer responsável por disparar mensagem para o Redis acionando o spider de pegar produtos.

---
+++ producers/get_products.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from tasks.worker import process_scraping_task
+
+
+payload = {
+    "usuario": "juliano@farmaprevonline.com.br",
+    "senha": "a007299A",
+    "callback_url": "https://desafio.cotefacil.net"
+}
+
+result = process_scraping_task(payload)
+print(f"[INFO] Tarefa {result.task} enviada para fila com sucesso.")
+
+
